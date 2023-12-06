a8503e76579ca464ddeffedc04b09f9d0e594a71
Abu Taleb <abutaleb142@gmail.com>
2023-12-06 18:04:28 +0600

    reactive navigation

---
+++ constants/themes.jsx
@@ -0,0 +1,21 @@
+export const COLORS = {
+    primary: "#87CEEB",
+    secondary: "#2980B9",
+    base: "#D98880",
+    dark: "#17202A",
+    alert: "#F39C12",
+    green:"#239B56",
+    white:"#ffffff",
+    btnPrimary: "#7D3C98",
+    btnSecondary: "#7D3C98",
+    btnSuccess: "#148F77"
+}
+
+export const SIZES = {
+    min: 6,
+    small: 11,
+    medium: 14,
+    large: 18,
+    extaLarge: 32,
+    font: 16
+}
